8e68dbc99d12ed6847bda73badea9e82f41d08db
Matthew Planchard <msplanchard@gmail.com>
2017-05-31 10:52:33 -0500

    moved project template to python_skeleton repo

--- python/empty_project/src/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Main package
-"""
-
-from ._version import __version__, __version_info__
--- python/empty_project/tests/test_nothing.py
+++
@@ -1,9 +0,0 @@
-"""
-Test nothing, so that ``tox`` does not exit with an error code
-"""
-
-
-def test_nothing():
-    """Tests that Python is running"""
-    pass
-
